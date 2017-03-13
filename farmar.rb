0bfd558a6285d7c89190b9a89a0fbbfe6491eefa
Chris <chris@adadevelopersacademy.org>
2017-03-13 12:04:24 -0700

    added farmar.rb

---
+++ farmar.rb
@@ -0,0 +1,9 @@
+# gems your project needs
+require 'csv'
+
+# our namespace module
+module FarMar; end
+
+# all of our data classes that live in the module
+require 'lib/farmar_market'
+# ...require all needed classes
